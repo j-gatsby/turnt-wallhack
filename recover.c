bd070f36c88265a5a42bae93e840da83b09466b1
j-gatsby <timjoeckel@gmail.com>
2014-07-11 04:32:47 -0400

    Add pseudocode before stopping for the night

--- recover.c
+++ recover.c
@@ -29,18 +29,36 @@ int main(int argc, char* argv[])
    // total number of blocks read off of card
     int blocks_read = 0;
     int bytes_read;
+    int jpegs_found = 0;
     
-    // find start of JPEGs
-    for (bytes_read =0;  bytes_read < BLOCK; bytes_read++)
+    do
     {
-    	// read card to buffer, one byte at a time
-    	fread(&buffer[bytes_read], sizeof(BYTE), 1, card);
-    	
-    } 
-    blocks_read++;
-    
-    printf("Bytes read = %d\n", bytes_read);
-    printf("Blocks read = %d\n", blocks_read);
+		for (bytes_read =0;  bytes_read < BLOCK; bytes_read++)
+		{
+			// read card to buffer, one byte at a time
+			fread(&buffer[bytes_read], sizeof(BYTE), 1, card);
+		} 
+		blocks_read++;
+		
+		printf("Bytes read = %d\n", bytes_read);
+		printf("Blocks read = %d\n", blocks_read);
+		
+		// find start of JPEG
+		if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] == 0xe0 || 0xe1))
+		{
+			// close any previous file being written to
+			// sprintf and create new file to store jpg
+			// increment jpegs_found
+		}
+		else if (// you are writing to a jpg file)
+		{
+			// keep writing to it
+		}
+		else
+		{
+			// just move on to the next block
+		}
+    }// until the end of the file is reached
     
   // close memory card file
   fclose(card);  
