e38ea2a78dd101751ae81c46b7556e74a791d658
j-gatsby <timjoeckel@gmail.com>
2014-07-11 02:29:28 -0400

    Change BLOCK from constant to typedef, and add blocks_read

--- recover.c
+++ recover.c
@@ -8,9 +8,11 @@
  */
  #include <stdio.h>
  
- #define BLOCK 512
+ #define JPEG_SIG0 0xff 0xd8 0xff 0xe0*
+ #define JPEG_SIG1 0xff 0xd8 0xff 0xe1*
  
  typedef uint8_t  BYTE;
+ typedef{uint8_t*512} BLOCK;
 
 int main(int argc, char* argv[])
 {
@@ -24,6 +26,9 @@ int main(int argc, char* argv[])
     
     // temporary storage
    BYTE buffer;
+   
+   // total number of blocks read off of card
+    int blocks_read = 0;
     
     // read card to buffer, one block at a time
     fread(&buffer, sizeof(BYTE), BLOCK, card);
